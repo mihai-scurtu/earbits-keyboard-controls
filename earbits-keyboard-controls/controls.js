6a33b6cfab1ef0e1043e5396571a2bf6a72cc115
Mihai Scurtu <mscurtu90@gmail.com>
2014-06-26 15:43:22 +0300

    Further attemps at volume control.

--- earbits-keyboard-controls/controls.js
+++ earbits-keyboard-controls/controls.js
@@ -1,33 +1,69 @@
-var keys = {
-	SPACE: 32,
-	LEFT: 37,
-	RIGHT: 39,
-	F: 70,
+var options = {
+	keys: {
+		SPACE: 32,
+		LEFT: 37,
+		RIGHT: 39,
+		F: 70
+	},
+	volumeIncrement: 5
 } 
 
 function noInputFocus() {
 	return !($('input[type=text]:focus, input[type=password]:focus').size());
 }
 
-$(function() {
+function setVolume(value, slider) {
+	var volSlider = /*slider ? slider : */$('.volume-slider');
+
+	volSlider.slider('value', value);
+	volSlider.slider('option', 'slide')(null, {value: volSlider.slider('value')});
+}
+
+function adjustVolume(delta, slider) {
+	var volSlider = /*slider ? slider : */$('.volume-slider');
+	console.log(volSlider.slider('value'));
+	var value = Math.min(Math.max(volSlider.slider('value') + delta, 0), 100);;
+
+	setVolume(value, volSlider);
+}
+
+$(window).load(function() {
 	// disable space scroll
 	$(document).on('keydown', function(e) {
-		if(e.which == keys.SPACE && noInputFocus()) {
+		if(e.which == options.keys.SPACE && noInputFocus()) {
 			return false;
 		}
 	});
 
 	$(document).on('keyup', function(e) {
 		if(noInputFocus()) {
-			if(e.which == keys.SPACE) {
+			if(e.which == options.keys.SPACE) {
 				$('.btn-playpause').eq(0).click();
-			} else if(e.ctrlKey && e.which == keys.RIGHT) {
+			} else if(e.ctrlKey && e.which == options.keys.RIGHT) {
 				$('.btn-skip').eq(0).click();
-			} else if(e.ctrlKey && e.which == keys.LEFT) {
+			} else if(e.ctrlKey && e.which == options.keys.LEFT) {
 				$('.btn-rewind').eq(0).click();
-			} else if(e.which == keys.F) {
+			} else if(e.which == options.keys.F) {
 				$('.btn-fullscreen').eq(0).click();
 			}
 		}
 	});
+
+	var bindVolumeInterval = setInterval(function() {
+		if(typeof $('.volume-slider').slider != 'undefined') {
+			$('#menu, #audio-controls').on('mousewheel', function(e) {
+				e.preventDefault();
+				// console.log(e);
+				if(parseInt(e.deltaY) > 0) {
+					adjustVolume(options.volumeIncrement);
+				} else {
+					adjustVolume(-options.volumeIncrement);
+				}
+			});
+			clearInterval(bindVolumeInterval);
+			console.log('volume-bind ok');
+		} else {
+			console.log('volume-bind fail');
+		}
+	}, 1000);
 });
